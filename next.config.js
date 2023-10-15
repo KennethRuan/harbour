0ea64f8ba202cb768bf5fc581b0370a6e0ef122c
Kenneth Ruan <kennethjruan@gmail.com>
2023-10-15 01:19:36 -0400

    Build fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-    preferRelative: true
-  },
 }
 
 module.exports = nextConfig
